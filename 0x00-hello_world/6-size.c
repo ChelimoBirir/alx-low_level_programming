9b09af9fdd016461db2ba503c9fa57661c68e78c
ChelimoBirir <Jelimobirir@gmail.com>
2023-02-16 03:07:48 -0800

    block comment

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,9 @@
  */
 
 
-/* prints sizes of various data types */ int main(void)
+/* 
+ * prints sizes of various data types 
+ */ int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
