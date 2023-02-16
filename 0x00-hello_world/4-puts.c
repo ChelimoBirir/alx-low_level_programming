c9135e4b9612c93bbdf18ebd7a7f4fe9cfd5913f
ChelimoBirir <Jelimobirir@gmail.com>
2023-02-16 02:37:38 -0800

    c exercises

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * This is a program that prints a string to stdout
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
