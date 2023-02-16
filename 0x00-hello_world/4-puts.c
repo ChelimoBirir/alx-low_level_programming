a22bff0ddedee5f9b67a925181fe04579019bda3
ChelimoBirir <Jelimobirir@gmail.com>
2023-02-16 02:52:58 -0800

    betty style again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 /**
- * This is a program that prints a string to stdout
  * main - Entry point
  * Return: Always 0(Success)
  */
 
 int main(void)
+/**
+ * This program prints a string to stdout
+ */
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
