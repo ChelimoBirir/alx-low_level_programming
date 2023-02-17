8e292724359ee84847ab5478d43fff991ad5f19e
ChelimoBirir <Jelimobirir@gmail.com>
2023-02-17 03:40:43 -0800

    overhaul

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,38 +8,40 @@
  * prints all possible combinations of two two-digit numbers
  */ int main(void)
 {
+	int i, j, k, m;
 
-	int tens;
-	int ones;
-	int tens1;
-	int ones1;
-
-	for (tens = '0'; tens <= '9'; tens++) /* prints first two digit combo */
+	i = 48;
+	while (i < 58)
 	{
-		for (ones = '0'; ones <= '9'; ones++)
+		j = 48;
+		while (j < 58)
 		{
-			for (tens1 = tens; tens1 <= '9'; tens1++) /* print second pair */
+			m = j + 1;
+			k = i;
+			while (k < 58)
 			{
-				for (ones1 = ones + 1; ones1 <= '9'; ones1++)
+				while (m < 58)
 				{
-					putchar(tens);
-					putchar(ones);
-					putchar(' ');
-					putchar(tens1);
-					putchar(ones1);
-
-					if (!((tens == '9' && ones == '8') &&
-					      (tens1 == '9' && ones1 == '9')))
+					putchar(i);
+					putchar(j);
+					putchar(32);
+					putchar(k);
+					putchar(m);
+					if (i < 57 || j < 56 || k < 57 || m < 57)
 					{
-						putchar(',');
-						putchar(' ');
+						putchar(44);
+						putchar(32);
 					}
+					m++;
 				}
-				ones1 = '0';
+				m = 48;
+				k++;
 			}
+			j++;
 		}
+		i++;
 	}
-	putchar('\n');
+	putchar(10);
 
 	return (0);
 }
