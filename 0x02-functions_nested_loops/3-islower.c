19a77434cec96556997877d88dadbd4261972ee3
ChelimoBirir <Jelimobirir@gmail.com>
2023-02-21 01:34:15 -0800

    batch 1

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,15 +7,18 @@
  *
  * Checks for lowercase character
  *
- */ int _islower(int c)
+ */ int main(void)
 {
-	if (c >= 'a' && c <= 'z')
+	int _islower(int c)
 	{
-		return (1);
-	}
-	else
-	{
-		return (0);
+		if (c >= 'a' && c <= 'z')
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
 	}
 }
 
