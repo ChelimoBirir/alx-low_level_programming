7a4ba77c52151c33c093f57edc6b990228fea9ed
ChelimoBirir <Jelimobirir@gmail.com>
2023-02-21 02:56:42 -0800

    batch 3

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/*
+ * print_to_98 - Entry point
+ *
+ * Return: void
+ *
+ * prints numbers from n to 98
+ *
+ */ void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+		n++;
+	}
+	else
+	{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+		n--;
+	}
+}
