4c3305ff1485b5d461cbe8c4cb0fdfa4abb596cf
ChelimoBirir <Jelimobirir@gmail.com>
2023-02-21 03:20:06 -0800

    ah

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@
 	{
 		while (n < 98)
 		{
-			_putchar('%d', n);
+			_putchar(48 + n);
 			_putchar(',');
 			_putchar(' ');
 			n++;
@@ -23,7 +23,7 @@
 	{
 		while (n > 98)
 		{
-			_putchar('%d', n);
+			_putchar(48 + n);
 			_putchar(',');
 			_putchar(' ');
 			n--;
