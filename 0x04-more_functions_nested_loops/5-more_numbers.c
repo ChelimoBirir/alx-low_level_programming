180e24a1ee25a250b014d21ee0bc160129ef1eda
ChelimoBirir <Jelimobirir@gmail.com>
2023-02-23 01:30:07 -0800

    repaired compilation errors

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,17 +8,22 @@
  */void more_numbers(void)
 {
 	int i;
-	int j;
+	char j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		j = 0;
 
 		while (j < 14)
 		{
-			_putchar(j);
+			if (j / 10 > 0)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
 			j++;
 		}
+		_putchar('\n');
 	}
 
 }
