5893c1ac2a5143a474fa0e444a7d48caafcac1d6
ChelimoBirir <Jelimobirir@gmail.com>
2023-02-23 01:31:43 -0800

    overhaul

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,23 +7,17 @@
  *
  */void more_numbers(void)
 {
-	int i;
-	char j;
+	char i;
+	int counter;
 
-	for (i = 0; i <= 9; i++)
+	for (counter = 1; counter <= 10; counter++)
 	{
-		j = 0;
-
-		while (j < 14)
+		for (i = 0; i <= 14; i++)
 		{
-			if (j / 10 > 0)
-			{
-				_putchar((j / 10) + '0');
-			}
-			_putchar((j % 10) + '0');
-			j++;
+			if (i / 10 > 0)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}
-
 }
