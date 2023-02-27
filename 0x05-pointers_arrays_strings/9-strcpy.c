a3128731e25063b844d50800294dff778a5f3779
ChelimoBirir <Jelimobirir@gmail.com>
2023-02-27 03:58:35 -0800

    defined _strlen function

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,23 @@
 #include "main.h"
 
+
+/*
+ * _strlen: returns the lenghth of a string
+ *
+ * Return: int
+ *
+ */int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
 /*
  * *_strcpy: copies a string into another
  *
