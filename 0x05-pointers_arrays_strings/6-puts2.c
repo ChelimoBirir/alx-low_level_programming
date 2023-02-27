ed1d76c2794af0665b709f5673da81ba5137f0ac
ChelimoBirir <Jelimobirir@gmail.com>
2023-02-27 03:26:42 -0800

    added logic case for null character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,31 @@
 #include "main.h"
 
 /*
+ * _strlen - finds length of s
+ *
+ * returns: lenght (i)
+ *
+ */int _strlen(char *s)
+{
+	int j = 0;
+
+	while (s[j] != '\0')
+	{
+		j++;
+	}
+	return (j);
+}
+
+/*
  * puts2: prints every other character of a string
  *
  * return: void
  *
- */void puts2(char *s)
+ */void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	while (s[i] != '\0')
+	while (str[i] != '\0' && i < _strlen(str))
 	{
 		_putchar(s[i]);
 		i += 2;
