2d9a13f37aa29e1c37c9f309f19f62203dbdc9c1
ChelimoBirir <Jelimobirir@gmail.com>
2023-02-27 04:01:34 -0800

    spaces

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,12 @@
 
 	for (i = 0; i <= n; i++)
 	{
-		printf("%d", a[i]);
+		if (i != (n - 1))
+			
+			printf("%d ,", a[i]);
 
-		while (i != n)
-		{
-			printf(", ");
-		}
+		else
+		
+			printf("%d", a[i]);
 	}
 }
