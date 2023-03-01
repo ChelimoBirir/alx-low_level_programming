f105f2738a846cfa15bd1449ba3f98999a8f9aa6
ChelimoBirir <Jelimobirir@gmail.com>
2023-03-01 12:51:47 -0800

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,26 +1,27 @@
 #include "main.h"
+
 /*
  * rot13 - encodes a string
  *
  * Return: pointer to the string
  *
- */char *rot13(char *str) 
+ */char *rot13(char *str)
 {
-    char *p = str;
-    int i;
+	char *p = str;
+	int i;
 
-    while (*p != '\0') 
+	while (*p != '\0')
 	{
-		if ((*p >= 'A' && *p <= 'M') || (*p >= 'a' && *p <= 'm')) 
+		if ((*p >= 'A' && *p <= 'M') || (*p >= 'a' && *p <= 'm'))
 		{
 			*p += 13;
-		} 
+		}
 		else if ((*p >= 'N' && *p <= 'Z') || (*p >= 'n' && *p <= 'z'))
 		{
 			*p -= 13;
 		}
 		p++;
-    	}
+	}
 
-    return str;
+	return (str);
 }
