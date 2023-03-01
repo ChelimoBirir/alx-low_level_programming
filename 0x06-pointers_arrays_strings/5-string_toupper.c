288eca4b92219a471b8004aa8ef65c4a6ee78c48
ChelimoBirir <Jelimobirir@gmail.com>
2023-03-01 10:39:26 -0800

    toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ * string_toupper - cnahges lowercase letters in a string to uppercase
+ *
+ * Return: string pointer
+ *
+ */char *string_toupper(char *)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i - 32];
+		}
+		i++
+	}
+	return (s);
+}
