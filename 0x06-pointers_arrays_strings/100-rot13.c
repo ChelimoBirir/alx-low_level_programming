95f4b26b8a9ae5912a014fa0586710882534ae7b
ChelimoBirir <Jelimobirir@gmail.com>
2023-03-01 12:44:12 -0800

    gpt

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,31 +4,23 @@
  *
  * Return: pointer to the string
  *
- */char *rot13(char *s)
+ */char *rot13(char *str) 
 {
+    char *p = str;
+    int i;
 
-	int a[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-		     'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-		     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-		     'W', 'X', 'Y', 'Z'};
-	int b[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-		     'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-		     'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-		     'J', 'K', 'L', 'M'};
-
-	int i, j;
-
-	for (j = 0; s[j] != '\0'; j++) /*loop through string*/
+    while (*p != '\0') 
 	{
-		i = 0;
-		while (a[i] != '\0' && s[j] != a[i]) /*loop through rot13 arr*/
-			i++;
+		if ((*p >= 'A' && *p <= 'M') || (*p >= 'a' && *p <= 'm')) 
+		{
+			*p += 13;
+		} 
+		else if ((*p >= 'N' && *p <= 'Z') || (*p >= 'n' && *p <= 'z'))
+		{
+			*p -= 13;
+		}
+		p++;
+    	}
 
-		if (s[j] == a[i]) /*if alpha matches, set to index in b arr*/
-			s[j] = b[i];
-	}
-
-	return (s);
+    return str;
 }
