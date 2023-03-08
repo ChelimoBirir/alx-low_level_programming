c7f7191f0e63d46ef924ab5b0073303f94ac0812
ChelimoBirir <Jelimobirir@gmail.com>
2023-03-08 01:27:37 -0800

    betty complained

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,7 +7,7 @@
  *
  */void _puts_recursion(char *s)
 {
-	
+
 	if (*s == '\0')
 	{
 		_putchar('\n');
@@ -15,5 +15,5 @@
 	}
 	_putchar(*s);
 	s++;
-       	_puts_recursion(s);
+	_puts_recursion(s);
 }
