73938548e7180e4000c3e8109b6f8beb497e31fe
ChelimoBirir <Jelimobirir@gmail.com>
2023-03-08 01:43:02 -0800

    removed pointless declaration

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,11 +7,9 @@
  *
  */void _print_rev_recursion(char *s)
 {
-	int i = _strlen(s);
 
-	if (i == 0)
+	if (*s == '\0')
 		return;
-	_putchar(s[i]);
-	s--;
-	_print_rev_recursion(s);
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
