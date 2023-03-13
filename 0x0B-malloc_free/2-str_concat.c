6715b74b4e4e8b26d13caa41134aa8a7e96fc0f1
ChelimoBirir <Jelimobirir@gmail.com>
2023-03-13 02:12:10 -0700

    overhaul

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,42 +9,37 @@
  *
  */char *str_concat(char *s1, char *s2)
 {
+	int len1, len2, len3, i;
 	char *new_str;
-
-	int i = 0, len = 0, j = 0, len2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[i] != '\0')
+	len1 = 0;
+	while (*(s1 + len1) != '\0')
 	{
-		len++;
-		i++;
+		len1++;
 	}
 
-	while (s2[j] != '\0')
+	len2 = 0;
+	while (*(s2 + len2) != '\0')
 	{
-		j++;
 		len2++;
 	}
-	len2++;
 
-	new_str = malloc(sizeof(char) * (len + len2));
+	len3 = len1 + len2;
 
-	while (i < len)
-	{
+	new_str = (char *) malloc(len3 * sizeof(char) + 1);
+
+	if (new_str == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
 		new_str[i] = s1[i];
-		i++;
-	}
-
-	while (j < len2)
-	{
-		new_str[i] = s2[j];
-		i++;
-		j++;
-	}
+	for (i = 0; i < len2; i++)
+		new_str[i + len1] = s2[i];
 
 	return (new_str);
 }
