7bcc921a6cc7f03a667743664a03fba8bacff573
ChelimoBirir <Jelimobirir@gmail.com>
2023-03-13 02:18:18 -0700

    deleted swap file

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 
 /*
- * argstostr - concatenates stdin arguments
+ * argstonew_str - concatenates stdin arguments
  *
- * Return: pointer to the string
+ * Return: pointer to the new_string
  *
- */int _strlen(char *string)
+ */int _strlen(char *s)
 {
 	int length;
 
 	length = 0;
-	while (string[length] != '\0')
+	while (s[length] != '\0')
 	{
 		length++;
 	}
@@ -22,13 +22,13 @@
  * argstostr - concatenates all the arguments to a program
  * @ac: argc - number of arguments
  * @av: argv - array of arguments
- * Return: pointe to new string
+ * Return: pointe to new new_string
  */
 
 char *argstostr(int ac, char **av)
 {
 	int length, i, j, k;
-	char *str;
+	char *new_str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
@@ -36,24 +36,24 @@ char *argstostr(int ac, char **av)
 	length = 0;
 	for (i = 0; i < ac; i++)
 	{
-		length += _strlen(av[i]) + 1;
+		length += _new_strlen(av[i]) + 1;
 	}
 
-	str = malloc(sizeof(char) * length + 1);
-	if (str == NULL)
+	new_str = malloc(sizeof(char) * length + 1);
+	if (new_str == NULL)
 		return (NULL);
 
 	k = 0;
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; j < _strlen(av[i]); j++)
+		for (j = 0; j < _new_strlen(av[i]); j++)
 		{
-			str[k] = av[i][j];
+			new_str[k] = av[i][j];
 			k++;
 		}
-		str[k] = '\n';
+		new_str[k] = '\n';
 		k++;
 	}
-	str[k] = '\0';
-	return (str);
+	new_str[k] = '\0';
+	return (new_str);
 }
