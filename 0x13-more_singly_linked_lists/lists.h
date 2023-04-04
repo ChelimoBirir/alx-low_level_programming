4db2884ffa1e33616bef04f81cfda62f1280796a
ChelimoBirir <Jelimobirir@gmail.com>
2023-04-04 03:34:07 -0700

    betty

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,8 +21,8 @@ typedef struct list_s
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 
@@ -46,5 +46,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);listint_t *find_listint_loop(listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 #endif
