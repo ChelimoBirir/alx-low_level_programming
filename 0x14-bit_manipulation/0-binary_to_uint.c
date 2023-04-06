403fcc61a60dd9561500457ec6d27a314f47515c
ChelimoBirir <Jelimobirir@gmail.com>
2023-04-06 05:06:38 -0700

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "bitwise.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned integer
+ *
+ * @b - pointer to the binary number
+ *
+ * Return - result(unisgned int)
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (int i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			result = (result << 1) + (b[i] - '0');
+		}
+		else
+			return (0);
+	}
+
+	return (result);
+}
