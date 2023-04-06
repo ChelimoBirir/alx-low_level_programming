2cd4645439fa3042a95e4a831076eff94c415935
ChelimoBirir <Jelimobirir@gmail.com>
2023-04-06 05:48:03 -0700

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,14 +9,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(n) * 8)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	unsigned long int mask = 1UL << index;
-
-	if (n & mask)
-		return (1);
-
-	else
+	if ((n & (1 << index)) == 0)
 		return (0);
+
+	return (1);
 }
