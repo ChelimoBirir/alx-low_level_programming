896affb1843cf51de94bed0a696858a565096e81
ChelimoBirir <Jelimobirir@gmail.com>
2023-05-04 03:38:15 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,44 @@
 #include "main.h"
 
 /**
+ * _strlen - returns length of string (modified)
+ * @s: string to be determined
+ *
+ * Return: length of string
+ */
+
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
+
+/**
+ * power - calculates the exponents
+ * @base: base
+ * @exp: exponent
+ *
+ * Return: result (int)
+ */
+
+int power(int base, int exp)
+{
+	int i, num;
+
+	num = 1;
+	for (i = 0; i < exp; ++i)
+		num *= base;
+
+	return (num);
+}
+
+/**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: pointer to the string representation of the binary number
  *
@@ -10,19 +48,19 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	int i;
+	int i, len;
 
 	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i] != '\0'; i++)
+	len = _strlen(b);
+
+	for (i = len - 1; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 
-		num <<= 1; /* shift left by 1 bit */
-		if (b[i] == '1')
-			num += 1; /* add 1 if current bit is 1 */
+		num += (b[i] - '0') * power(2, len - i - 1);
 	}
 
 	return (num);
